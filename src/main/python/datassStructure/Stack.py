69d21667864226f922fb0fcb2801dbab530d5c97
Leonardo Ji <leonardo.ji@cerner.com>
2021-10-19 15:01:24 -0500

    reorganize

---
+++ src/main/python/datassStructure/Stack.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+myStack = deque()
+
+myStack.append('a')
+myStack.append('b')
+myStack.append('c')
+
+deque(['a', 'b', 'c'])
+
+print(myStack.pop())
+
+print(myStack.pop())
+
+print(myStack.pop())
