2933fb97a7c93b0cab5e818bffa6a90db81856c0
lj015625 <leonardoji80@gmail.com>
2021-10-19 00:06:50 -0500

    Update

--- src/main/python/leetcode/sortNum.py
+++ src/main/python/leetcode/sortNum.py
@@ -4,7 +4,7 @@ class Solution:
         n = a[0]
         # remove the first number which is length
         a = a[1:n+1]
-        sum = 0
+        total = 0
         for i in range(0, n):
             temp = a[i]
             j = i - 1
@@ -17,9 +17,9 @@ class Solution:
 
             print(a[0:i+1])
             for k in range(0, i+1):
-                sum += a[k] * (k+1)
+                total += a[k] * (k + 1)
 
-        return sum
+        return total
 
 
 
