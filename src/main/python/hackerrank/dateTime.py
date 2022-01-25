9a9376242a7ca2f5518419c5503245c43d01759d
Leonardo Ji <leonardo.ji@cerner.com>
2022-01-24 22:09:16 -0600

    Add my code

--- src/main/python/hackerrank/dateTime.py
+++ src/main/python/hackerrank/dateTime.py
@@ -3,6 +3,6 @@ from datetime import datetime as dt
 def time_delta(t1, t2):
     """ absolute seconds between to time stamps """
     fmt = '%a %d %b %Y %H:%M:%S %z'
-    seconds = (dt.strptime(input(), fmt).total_seconds() - dt.strptime(input(), fmt)).total_seconds()
-    return str(int(abs(seconds)))
+    t = (dt.strptime(t1, fmt) - dt.strptime(t2, fmt)).total_seconds()
+    return str(int(abs(t)))
 
---
+++ src/main/python/hackerrank/polynominal.py
@@ -0,0 +1,4 @@
+
+# evaluate polynomial
+x,k=1,4
+print (k==eval('x**3 + x**2 + x + 1'))
