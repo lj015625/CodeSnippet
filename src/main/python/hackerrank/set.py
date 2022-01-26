f883893ab885d5fb85af31875938275d04e37786
Leonardo Ji <leonardo.ji@cerner.com>
2022-01-26 00:19:49 -0600

    add code

--- src/main/python/hackerrank/set.py
+++ src/main/python/hackerrank/set.py
@@ -59,3 +59,9 @@ R = set("Rank")
 H.symmetric_difference_update(R)
 print(H)
 #set(['c', 'e', 'H', 'n', 'r', 'R'])
+
+# all numbers repeating five time except one number.  Find that number.
+k = 5
+arr = [1, 2, 3, 6, 5, 4, 4, 2, 5, 3, 6, 1, 6, 5, 3, 2, 4, 1, 2, 5, 1, 4, 3, 6, 8, 4, 3, 1, 5, 6, 2]
+myset = set(arr)
+print(((sum(myset)*k)-(sum(arr))) // (k-1))
