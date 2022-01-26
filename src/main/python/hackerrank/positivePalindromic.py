fa288df33dbbc4c9ba740929f3f790b4ef7b36c6
Leonardo Ji <leonardo.ji@cerner.com>
2022-01-25 23:25:40 -0600

    add comment

--- src/main/python/hackerrank/positivePalindromic.py
+++ src/main/python/hackerrank/positivePalindromic.py
@@ -1,7 +1,8 @@
-
 def positive_palindromic(arr):
+    '''all positive and any number is palindrome '''
     palindromic = any([j == j[::-1] for j in arr])
     pos = (all([int(i) > 0 for i in arr]))
     return pos and palindromic
 
-print(positive_palindromic(['1', '2', '3', '4', '5', '-9']))
+# 121 is palindrome while 121 is not.
+print(positive_palindromic(['123', '121']))
