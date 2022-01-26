4efe3e3bfe0ed8e42b64df80160b6164fbc6a7ec
Leonardo Ji <leonardo.ji@cerner.com>
2022-01-26 17:43:00 -0600

    add code

--- src/main/python/hackerrank/decorator.py
+++ src/main/python/hackerrank/decorator.py
@@ -2,10 +2,9 @@
 def person_lister(f):
     """sort persons by age"""
     def inner(persons):
-        # return map(f, sorted(people, key=lambda x: x[2]))
-        for ps in sorted(persons, key=lambda x: int(x[2])):
-            yield f(ps)
-
+        # for ps in sorted(persons, key=lambda x: int(x[2])):
+        #     yield f(ps)
+        return map(f, sorted(persons, key=lambda x: x[2]))
     return inner
 
 @person_lister
@@ -16,3 +15,25 @@ if __name__ == '__main__':
 
     persons = [['Mike', 'Thomson', '20', 'M'], ['Robert', 'Bustle', '32', 'M'], ['Andria', 'Bustle', '30', 'F']]
     print(*name_format(persons), sep='\n')
+
+
+
+def wrapper(f):
+    """format phone number to add +91"""
+    def fun(l):
+        # from right to left las 5 digits, last 5 to last 10 digits, then add +91 to the left
+        for c in l:
+            f(["+91 "+c[-10:-5]+" "+c[-5:]])
+        return l
+
+    return fun
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = ['07895462130',
+         '919875641230',
+         '9195969878']
+    sort_phone(l)
