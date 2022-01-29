8ded5975c6ff9d550ec4ceade55eeaab6ca0313a
Leonardo Ji <leonardo.ji@cerner.com>
2022-01-29 00:33:41 -0600

    add code

---
+++ src/main/python/questions/stringShift.py
@@ -0,0 +1,26 @@
+"""Given two strings A and B, write a function can_shift to return
+whether or not A can be shifted some number of places to get B."""
+
+# def can_shift(string1, string2):
+#     if len(string1) != len(string2):
+#         return False
+#     for i in range(len(string1)):
+#         string1 = string1[-1] + string1[:-1]
+#         if string1 == string2:
+#             return True
+#     return False
+
+def can_shift(string1, string2):
+    # A = 'abcde'
+    # B * 2 = 'cdeabcdeab'
+    return (string1 and string2 and
+             len(string1) == len(string2) and
+            string1 in string2 * 2)
+
+A = 'abcde'
+B = 'cdeab'
+assert(can_shift(A, B) == True)
+
+A = 'abc'
+B = 'acb'
+assert(can_shift(A, B) == False)
