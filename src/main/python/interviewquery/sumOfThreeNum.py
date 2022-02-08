fc2c41e8e5ca4622575e53ada96ae480b333f976
Leonardo Ji <leonardo.ji@cerner.com>
2022-02-08 00:23:12 -0600

    refactor

---
+++ src/main/python/interviewquery/sumOfThreeNum.py
@@ -0,0 +1,19 @@
+def threeNumberSum(array, targetSum):
+    results = []
+    array.sort()
+    for i in range(len(array) - 2):
+        left = i + 1
+        right = len(array) - 1
+        while left < right:
+            currentSum = array[i] + array[left] + array[right]
+            if currentSum == targetSum:
+                results.append([array[i], array[left], array[right]])
+                left += 1
+                right -= 1
+            elif currentSum < targetSum:
+                left += 1
+            elif currentSum > targetSum:
+                right -= 1
+    return results
+
+print(threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0))
