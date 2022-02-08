03b2e1b4e5c08b34a4eead0755ba5cdc4a5cea98
Leonardo Ji <leonardo.ji@cerner.com>
2022-02-07 23:56:05 -0600

    refactor

--- src/main/python/interviewquery/sumToTarget.py
+++ src/main/python/interviewquery/sumToTarget.py
@@ -16,3 +16,38 @@ def sum_pair_indices(array, target):
 array = [1, 2, 3, 4]
 target = 5
 print(sum_pair_indices(array, target))
+
+
+def twoNumberSum(array, targetSum):
+    # use hashset O(n) time O(n) space
+    saved = {}
+    for current_num in array:
+        complement = targetSum - current_num
+        if complement in saved:
+            return [current_num, complement]
+        else:
+            # use dict as a hashset not a hashmap
+            saved[current_num] = True
+    return []
+
+array = [3,5,-4,8,11,1,-1,6]
+target = 10
+print(twoNumberSum(array, target))
+
+
+def twoNumberSum2(array, targetSum):
+    # use sorting O(nlogn) time O(1) space
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
+    return []
+
+print(twoNumberSum2(array, target))
