ad9119a44f8d59b60cc2ad5affb985e071336c7b
Leonardo Ji <leonardo.ji@cerner.com>
2022-02-14 21:57:35 -0600

    refactor

---
+++ src/main/python/algo/stringCharShift.py
@@ -0,0 +1,28 @@
+"""Shift string characters by k position. letter should wrap around, z shifts one return a. """
+
+
+def caesarCipherEncryptor(string, key):
+    shiftedString = ""
+    for char in string:
+        unicode = (ord(char) - ord('a') + key) % 26 + ord('a')
+        shiftedString = shiftedString + chr(unicode)
+    return shiftedString
+
+
+def caesarCipherEncryptor2(string, key):
+    shifted = []
+    newKey = key % 26
+    alphabet = list('abcdefghijklmnopqrstuvwxyz')
+    for char in string:
+        newLetterCode = (alphabet.index(char) + newKey) % 26
+        shifted.append(alphabet[newLetterCode])
+    return ''.join(shifted)
+
+
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual("zab", caesarCipherEncryptor("xyz", 2))
+        self.assertEqual("zab", caesarCipherEncryptor2("xyz", 2))
