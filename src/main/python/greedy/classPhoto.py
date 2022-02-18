33ede7780a7d02f46667c5f9026a9ed5fb2a4697
Leonardo Ji <leonardo.ji@cerner.com>
2022-02-18 16:19:07 -0600

    refactor

---
+++ src/main/python/greedy/classPhoto.py
@@ -0,0 +1,39 @@
+"""
+Given two arrays redShirtHeights and blueShirtHeights arrange rows such that
+All students in red shirts must be in the same row.
+All students in blue shirts must be in the same row.
+Each student in back row must be taller than the student directly in front.
+"""
+
+
+def classPhotos(redShirtHeights, blueShirtHeights):
+    if len(redShirtHeights) != len(blueShirtHeights):
+        return False
+    # sort in reverse order then start with tallest pairing up one from each front row and back row.
+    redShirtHeights.sort(reverse=True)
+    blueShirtHeights.sort(reverse=True)
+
+    if redShirtHeights[0] > blueShirtHeights[0]:
+        for i, j in zip(redShirtHeights, blueShirtHeights):
+            # if back row's tallest student is shorter than front row student then return false
+            # because none of the other student will be taller than that.
+            if i <= j:
+                return False
+    else:
+        for i, j in zip(blueShirtHeights, redShirtHeights):
+            if i <= j:
+                return False
+
+    return True
+
+
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        blueShirtHeights = [5, 6, 7, 2, 3, 1, 2, 3]
+        redShirtHeights = [1, 1, 1, 1, 1, 1, 1, 1]
+        expected = True
+        actual = classPhotos(redShirtHeights, blueShirtHeights)
+        self.assertEqual(expected, actual)
