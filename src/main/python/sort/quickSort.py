3d942666a5987e1e6045888887bd4b6ef36f04d8
Leonardo Ji <leonardo.ji@cerner.com>
2022-02-21 23:43:23 -0600

    refactor

--- src/main/python/sort/quickSort.py
+++ src/main/python/sort/quickSort.py
@@ -1,4 +1,3 @@
-# Python program for Quicksort
 # This function takes last element as pivot, places
 # the pivot element at its correct position in sorted
 # array, and places all smaller (smaller than pivot)
@@ -6,7 +5,7 @@
 # of pivot
 def partition(arr, low, high):
     i = (low - 1)  # index of smaller element
-    pivot = arr[high]  # pivot
+    pivot = arr[high]  # pivot on the right
 
     for j in range(low, high):
 
@@ -15,8 +14,10 @@ def partition(arr, low, high):
         if arr[j] <= pivot:
             # increment index of smaller element
             i = i + 1
+            # swap smaller number than pivot to the left pointer
             arr[i], arr[j] = arr[j], arr[i]
 
+    # put pivot with final position of the right pointer
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
     return i + 1
 
@@ -25,8 +26,6 @@ def partition(arr, low, high):
 # arr[] --> Array to be sorted,
 # low --> Starting index,
 # high --> Ending index
-
-# Function to do Quick sort
 def quickSort(arr, low, high):
     if low < high:
         # pi is partitioning index, arr[p] is now
@@ -37,13 +36,43 @@ def quickSort(arr, low, high):
         # partition and after partition
         quickSort(arr, low, pi - 1)
         quickSort(arr, pi + 1, high)
+    return arr
 
-    # Driver code to test above
+
+# use left most as pivot
+# avg O(nlog(n)) time O(nlog(n)) space
+# worst O(n^2) time O(nlog(n)) space
+def quickSort2(array):
+    qucikSortHelper(array, 0, len(array) - 1)
+    return array
+
+
+def qucikSortHelper(array, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while right >= left:
+        if array[left] > array[pivot] > array[right]:
+            array[left], array[right] = array[right], array[left]
+        if array[left] <= array[pivot]:
+            left += 1
+        if array[right] >= array[pivot]:
+            right -= 1
+    array[pivot], array[right] = array[right], array[pivot]
+
+    leftSubarraySmaller = right - start < end - right
+    if leftSubarraySmaller:
+        qucikSortHelper(array, start, right - 1)
+        qucikSortHelper(array, right + 1, end)
+    else:
+        qucikSortHelper(array, right + 1, end)
+        qucikSortHelper(array, start, right - 1)
 
 
 arr = [10, 7, 8, 9, 1, 5]
-n = len(arr)
-quickSort(arr, 0, n - 1)
-print("Sorted array is:")
-for i in range(n):
-    print("%d" % arr[i]),
+print("Sorted array is: ", quickSort(arr, 0, len(arr) - 1))
+
+arr = [10, 7, 8, 9, 1, 5]
+print("Sorted array is: ", quickSort2(arr))
