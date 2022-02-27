4eb29cb73cfa1148b396cb9dbb7a1c160a0ca78c
Leonardo Ji <leonardo.ji@cerner.com>
2022-02-27 00:45:22 -0600

    refactor

--- src/main/python/dp/countBills.py
+++ src/main/python/dp/countBills.py
@@ -1,47 +1,78 @@
+"""
+Count number of ways from bills to sum to amount.
+"""
+
+
 # use Recursive functions
-def countways_(bills, amount, index):
+def countways_(amount, bills, index):
     if amount == 0:  # base case 1
         return 1
     if amount < 0 or index >= len(bills):  # base case 2
         return 0
     # count the amount with current bill, and amount without current bill
-    return countways_(bills, amount - bills[index], index) + countways_(bills, amount, index + 1)
+    return countways_(amount - bills[index], bills, index) + countways_(amount, bills, index + 1)
 
 
-def countways(bills, amount):
-    return countways_(bills, amount, 0)
-
-
-print(countways([1, 2, 5], 5))
+def countways(amount, bills):
+    return countways_(amount, bills, 0)
 
 
 # more efficient use dynamic programming
-def countways_dp(bills, amount):
+def countways_dp(amount, bills):
     if amount <= 0:
         return 0
     # save calculated results default to 1 for each amount
     dp = [[1 for _ in range(len(bills))] for _ in range(amount + 1)]
-    #print(dp)
+    # print(dp)
     for amt in range(1, amount + 1):
         for j in range(len(bills)):
             bill = bills[j]
-            # can add this bill j
+            # we can add this bill j because amt - bill >= 0
             if amt - bill >= 0:
                 # existing count for j and (amt - bill) is dp[amt - bill][j]
                 x = dp[amt - bill][j]
+            # we cannot add this bill j
             else:
                 x = 0
-            # cannot add this bill j
+            # count of amt previously without bill j
             if j >= 1:
                 # existing count for previous j-1 and amt is dp[amt][j-1]
                 y = dp[amt][j - 1]
             else:
                 y = 0
-            # save count for j and amt
+            # count is ways to count of (amt - bill) and count of (amt
             dp[amt][j] = x + y
 
-    print(dp)
+    # print(dp)
     return dp[amount][len(bills) - 1]
 
 
-print(countways_dp([1, 2, 5], 5))
+# much more efficient dp
+# O(nd) time O(n) space
+def numberOfWaysToMakeChange(amount, bills):
+    if amount <= 0:
+        return 0
+    ways = [0 for _ in range(amount + 1)]
+    ways[0] = 1
+    # for each bill add to ways
+    for bill in bills:
+        # for each amount from 1 to amount
+        for amt in range(1, amount + 1):
+            # if we can add this bill then count of (amt) is added by count of (amt - bill)
+            if bill <= amt:
+                ways[amt] += ways[amt - bill]
+    return ways[amount]
+
+
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(4, countways(5, [1, 2, 5]))
+
+    def test_case_2(self):
+        self.assertEqual(4, countways_dp(5, [1, 2, 5]))
+
+    def test_case_3(self):
+        self.assertEqual(4, numberOfWaysToMakeChange(5, [1, 2, 5]))
