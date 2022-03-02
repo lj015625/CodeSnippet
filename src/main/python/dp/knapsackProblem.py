5f81e3776b47ac3f7323964c26b665a5a9209047
Leonardo Ji <leonardo.ji@cerner.com>
2022-03-02 17:27:27 -0600

    refactor

--- src/main/python/dp/knapsackProblem.py
+++ src/main/python/dp/knapsackProblem.py
@@ -7,14 +7,20 @@ Your goal is find maximum combined value of items without exceed capacity.
 
 
 def knapsackProblem(items, capacity):
+    # bag of capacity [0, 1, 2...capacity + 1] for each items
     knapsackValues = [[0 for x in range(capacity + 1)] for y in range(len(items) + 1)]
+    # iterate over each items
     for i in range(1, len(items) + 1):
         currentWeight = items[i - 1][1]
         currentValue = items[i - 1][0]
+        # iterate over each capacities
         for c in range(capacity + 1):
+            # do no use this item
             if currentWeight > c:
                 knapsackValues[i][c] = knapsackValues[i - 1][c]
+            # use this item
             else:
+                # max value from either replace with current item or not with current item
                 knapsackValues[i][c] = max(knapsackValues[i - 1][c], knapsackValues[i - 1][c - currentWeight] + currentValue)
     return [knapsackValues[-1][-1], getKnapsackValues(knapsackValues, items)]
 
