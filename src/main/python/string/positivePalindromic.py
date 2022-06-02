0950edeb5558925e808ae77e96eca6a61f358cc5
Leonardo Ji <leonardo.ji@cerner.com>
2022-06-02 14:11:19 -0500

    refactor

--- src/main/python/string/positivePalindromic.py
+++ src/main/python/string/positivePalindromic.py
@@ -1,8 +1,11 @@
+"""
+Any number in array is all positive and anyone of them is palindrome.
+"""
 def positive_palindromic(arr):
     '''all positive and any number is palindrome '''
     palindromic = any([j == j[::-1] for j in arr])
     pos = (all([int(i) > 0 for i in arr]))
     return pos and palindromic
 
-# 121 is palindrome while 121 is not.
+# 121 is palindrome while 123 is not.
 print(positive_palindromic(['123', '121']))
