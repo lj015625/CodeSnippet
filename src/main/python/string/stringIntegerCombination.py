9b18955c791b0c0c2cb702479ec61e207c9df387
Leonardo Ji <leonardo.ji@cerner.com>
2022-06-05 18:52:27 -0500

    refactor

--- src/main/python/string/stringIntegerCombination.py
+++ src/main/python/string/stringIntegerCombination.py
@@ -1,12 +1,20 @@
 """Write a Python function that adds together
 all combinations of adjacent integers of a given string of integers named int_str."""
 
+
 def int_str_addition(int_str):
     sum = 0
     for i in range(len(int_str)):
-        for j in range(i+1, len(int_str)+1):
+        for j in range(i + 1, len(int_str) + 1):
+            print(int_str[i:j])
             sum += int(int_str[i:j])
     return sum
 
-int_str = '12'
-print(int_str_addition(int_str))
+
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        int_str = '12'
+        self.assertEqual(15, int_str_addition(int_str))
