a15980762b6dd03a12965c148d7d4111ef07d93c
Leonardo Ji <leonardo.ji@cerner.com>
2022-06-06 09:39:44 -0500

    refactor

--- src/main/python/string/wordsNumberOfVowels.py
+++ src/main/python/string/wordsNumberOfVowels.py
@@ -1,4 +1,4 @@
-"""The score of a single word is  if the word contains an even number of vowels. Otherwise, the score of this word is 1.
+"""The score of a single word is 2 if the word contains an even number of vowels. Otherwise, the score of this word is 1.
 The score for the whole list of words is the sum of scores of all words in the list.
 """
 
@@ -14,8 +14,10 @@ def score_words(words):
         for letter in word:
             if is_vowel(letter):
                 num_vowels += 1
+        # add 2 for even number of vowels
         if num_vowels % 2 == 0:
             score += 2
+        # add 1 for odd number of vowels
         else:
             score += 1
     return score
